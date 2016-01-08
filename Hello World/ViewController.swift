a32eaca4b5b4da2cd908475ba7c05d405cd89277
pazar <student@pazar.local>
2016-01-08 10:18:53 -0600

    initial commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by pazar on 8/21/15.
+//  Copyright (c) 2015 pazar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+          view.backgroundColor = UIColor .magentaColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+      
+    }
+
+
+}
+
